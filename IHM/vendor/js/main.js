7cf0080061219de766e8fd526e7fced91ec3497c
unknown <nina.batko@gmail.com>
2021-01-20 18:44:30 +0100

    Added PUT and POST requests

---
+++ IHM/vendor/js/main.js
@@ -0,0 +1,7 @@
+document.getElementById("myButtonNewSolution").onclick = function () {
+    location.href = "startResolution.html";
+};
+
+document.getElementById("myButtonChangeGANTT").onclick = function () {
+    location.href = "aleaGantt.html";
+};
--- vendor/js/main.js
+++ vendor/js/main.js
@@ -18,9 +18,47 @@ function myFunction() {
     });    
 }
 
+function formNewSolutionSubmit(){
+    var your_form = $("#new_solution_form");
 
-document.getElementById("buttonStartCalculus").onclick = function () {
-    location.href = "GANTT.html";
-};
+    var urlLink = "https://qrfx7lea3b.execute-api.eu-west-3.amazonaws.com/dev/project/parameters/";
+    urlLink = urlLink + $("#inputMeca1").val() + "/" + $("#inputQC1").val() + "/" + $("#inputMeca2").val() + "/" +$("#inputQC2").val() + "/" + $("#inputTimeout").val();
+
+    alert(urlLink)
+    $.post(urlLink,{},
+    function(data, status){
+        alert("Data: " + data + "\nStatus: " + status);
+    });
+}
+
+function formAleaSubmit(){
+    /*$('submitButtonAlea').click(function() {
+        $.post("https://qrfx7lea3b.execute-api.eu-west-3.amazonaws.com/dev/project/constraints",$("#alea_form").serialize(),
+            function(data, status){
+                alert("Data: " + data + "\nStatus: " + status);
+            });
+    });*/
+
+       //hang on event of form with id=myform
+       $("#alea_form").submit(function(e) {
+
+        //prevent Default functionality
+        e.preventDefault();
+
+        //get the action-url of the form
+        var actionurl = e.currentTarget.action;
+        //alert($("#alea_form").serialize())
+
+        //do your own request an handle the results
+        $.ajax({
+                url: "https://qrfx7lea3b.execute-api.eu-west-3.amazonaws.com/dev/project/constraints?" +$("#alea_form").serialize(),
+                type: 'PUT',
+                success: function(data) {
+                    alert("Alea added");
+                }
+        });
+
+    });
+}
 
 
